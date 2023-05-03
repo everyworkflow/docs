f0755b070baed5be9bf5d29861041d39a8494825
Suson Waiba <susonwaiba@gmail.com>
2023-05-03 21:56:34 +0545

    added @docusaurus/plugin-google-gtag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,6 +52,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-6B0LCVR5VL',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
